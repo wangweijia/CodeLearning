a2a126208fa4e0eed820728c46d93ef3f8249c6c
kwj <weijia.wang@eku001.com>
2016-07-19 18:36:17 +0800

    RAC学习

---
+++ iOS/RACSignal/RACSignal_MVVMTests/RACSignal_MVVMTests.m
@@ -0,0 +1,39 @@
+//
+//  RACSignal_MVVMTests.m
+//  RACSignal_MVVMTests
+//
+//  Created by apple on 7/6/16.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RACSignal_MVVMTests : XCTestCase
+
+@end
+
+@implementation RACSignal_MVVMTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
